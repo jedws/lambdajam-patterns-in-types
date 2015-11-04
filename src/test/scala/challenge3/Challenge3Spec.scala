c8283740ecf8068ebe691734ba0d4b5db2a2c7ae
Jed Wesley-Smith <jed@atlassian.com>
2015-11-04 11:34:49 +1100

    start migration to immutable specs

--- src/test/scala/challenge3/Challenge3Spec.scala
+++ src/test/scala/challenge3/Challenge3Spec.scala
@@ -2,26 +2,24 @@ package challenge3
 
 import challenge0._, EqualSyntax._
 
-object Challenge3Spec extends test.Spec {
+object Challenge3Spec extends test.Spec2 {
   import Laws._
   import Writer._
   import WriterArbitraries._
 
+  def is = s2"""
   "Writer" should {
-    "satisfy equal laws" ! equal.laws[Writer[List[Int], Int]]
+    satisfy equal laws     ${equal.laws[Writer[List[Int], Int]]}
+    satisfy monad laws     ${monad.laws[Writer_[List[Int]]#l]}
+    return zero for value  ${prop { (i: Int) => Writer.value[List[Int], Int](i) === Writer(Monoid[List[Int]].zero, i) }}
+    tell appends           $tellAppends
+    satisfy monoid laws"   ${monoid.laws[Writer[List[Int], List[Int]]]}
+  """
 
-    "satisfy monad laws" ! monad.laws[Writer_[List[Int]]#l]
+  def tellAppends = prop((i: List[Int], j: List[Int]) =>
+    (for {
+      _ <- tell(i)
+      _ <- tell(j)
+    } yield ()) === Writer(i ++ j, ()))
 
-    "return zero for value" ! prop { (i: Int) =>
-      Writer.value[List[Int], Int](i) === Writer(Monoid[List[Int]].zero, i)
-    }
-
-    "tell appends" ! prop((i: List[Int], j: List[Int]) =>
-      (for {
-        _ <- tell(i)
-        _ <- tell(j)
-      } yield ()) === Writer(i ++ j, ()))
-
-    "satisfy monoid laws" ! monoid.laws[Writer[List[Int], List[Int]]]
-  }
 }
--- src/test/scala/test/Spec.scala
+++ src/test/scala/test/Spec.scala
@@ -1,7 +1,11 @@
 package test
 
-import org.specs2.{mutable, ScalaCheck}, mutable._
+import org.specs2._
 
 abstract class Spec
+  extends mutable.Specification
+  with ScalaCheck
+
+abstract class Spec2
   extends Specification
   with ScalaCheck
